19e570148cf914fe6d84bd62c6003b2e1ebeadb4
james-e-crandall <james.e.crandall@gmail.com>
2025-07-15 18:05:56 -0500

    greate people need fixing

---
+++ ui/game.js
@@ -0,0 +1,50 @@
+window.UnitPortraitVisualRemaps = {
+    ...(window.UnitPortraitVisualRemaps || {}),
+    'UNIT_DOGE_ORSO_IPATO': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_DOGE_FRANCESCO_FOSCARI': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },  
+    'UNIT_DOGE_LUDOVICO_MANIN': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
+    'UNIT_DOGE_ORDELAFO_FALIERO': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },    
+    'UNIT_DOGE_BARTOLOMEO_GRADENIGO': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },      
+    'UNIT_DOGE_PIETRO_TRADONICO': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    }, 
+    'UNIT_DOGE_DOMENICO_II_CONTARINI': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },   
+    'UNIT_DOGE_SABASTIANO_VENIER': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },   
+    'UNIT_DOGE_FRANCESCO_MOROSINI': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },  
+    'UNIT_DOGE_ENRICO_DANDOLO': { //your unit type
+        unitType: 'UNIT_CONQUISTADOR_FERDINAND_MAGELLAN',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },  
+
+}
+
+
+
+
+
+
